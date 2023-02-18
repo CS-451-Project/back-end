4b2c2c261216e346392f8ce6df9c4a059eee2a2f
Austin Nguyen <austinkn123@gmail.com>
2023-02-18 16:36:10 -0600

    Added a AddBankAccount Controller

--- src/GivingCircle.Api/Controllers/BankAccountController.cs
+++ src/GivingCircle.Api/Controllers/BankAccountController.cs
@@ -1,4 +1,6 @@
 ﻿using GivingCircle.Api.Fundraiser.DataAccess;
+using GivingCircle.Api.Fundraiser.Models;
+using GivingCircle.Api.Fundraiser.Models.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,12 +18,46 @@ namespace GivingCircle.Api.Controllers
         private readonly ILogger<BankAccountController> _logger;
 
         private readonly IFundraiserRepository _fundraiserRepository;
+
         public BankAccountController(ILogger<BankAccountController> logger, IFundraiserRepository fundraiserRepository)
         {
             _logger = logger;
             _fundraiserRepository = fundraiserRepository;
 
         }
+
+        public async Task<IActionResult> AddBankAccount([FromBody] BankAccount bankaccount)
+        {
+            _logger.LogInformation("Received POST request");
+            try
+            {
+                // Create the bank account id
+                var bankaccountid = Guid.NewGuid().ToString();
+                BankAccount addBankAccount = new()
+                {
+                    Account_Name = bankaccount.Account_Name,
+                    Address = bankaccount.Address,
+                    City = bankaccount.City,
+                    State = bankaccount.State,
+                    Zipcode = bankaccount.Zipcode,
+                    Bank_Name = bankaccount.Bank_Name,
+                    Account_Num = bankaccount.Account_Num,
+                    Routing_Num = bankaccount.Routing_Num,
+                    Account_Type = bankaccount.Account_Type,
+                    Bank_Account_Id = bankaccountid,
+                  };
+                var result = await _fundraiserRepository.AddBankAccount(addBankAccount);
+            }
+            catch (Exception err)
+            {
+                _logger.LogError(err.Message);
+            }
+
+
+            return Ok();
+        }
+
+
 
     }
 }
