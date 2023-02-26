2326ee6edb278a9ae0b3dbc59203fe739bbe3f78
XploitedHntrGthrr <dhjpr7@umsystem.edu>
2023-02-26 15:17:36 -0600

    Changed validation to handle the login

--- src/GivingCircle.Api.DataAccess/Repositories/IUserRepository.cs
+++ src/GivingCircle.Api.DataAccess/Repositories/IUserRepository.cs
@@ -39,10 +39,10 @@ namespace GivingCircle.Api.DataAccess.Repositories
         /// <summary>
         /// Validates User
         /// </summary>
-        /// <param name="userId">The users id</param>
+        /// <param name="email">The users email</param>
         /// <param name="password"> The users password</param>
         /// <returns>True if validated, else false</returns>
-        Task<bool> ValidateUserIdAsync(string userId, string password);
+        Task<bool> ValidateUserAsync(string email, string password);
 
 
     }
